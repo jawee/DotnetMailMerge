c39ea4da6635360359dc22eee14e79c04ca19b12
Andreas Olsson <olsson.andreas@gmail.com>
2023-08-07 17:17:08 +0200

    began markdown

--- DotnetMailMerge.Tests/MarkdownLexerTests.cs
+++ DotnetMailMerge.Tests/MarkdownLexerTests.cs
@@ -1,9 +1,63 @@
-﻿namespace DotnetMailMerge.Tests;
+﻿using DotnetMailMerge.Markdown;
+using NUnit.Framework;
 
+namespace DotnetMailMerge.Tests;
+
+[TestFixture]
 public class MarkdownLexerTests
 {
-	public MarkdownLexerTests()
-	{
-	}
-}
+    [Test]
+    public void TestInitializeLexer_Ok()
+    {
+        var input = "";
+        _ = new Lexer(input);
+    }
 
+    [Test]
+    public void TestLexer() 
+    {
+        var testCases = new[] {
+            new TestCase("#", new[] { CreateToken(TokenType.Heading) }),
+            new TestCase("*", new[] { CreateToken(TokenType.Item)} ),
+            new TestCase("\n", new[] { CreateToken(TokenType.LineBreak)}),
+            new TestCase("a", new[] { CreateToken(TokenType.Letter, "a")}),
+            //new TestCase("{{p}}", new[] {
+            //    CreateToken(TokenType.Start, "{{"),
+            //    CreateToken(TokenType.Character, "p"),
+            //    CreateToken(TokenType.End, "}}"),
+            //    CreateToken(TokenType.EOF),
+            //}),
+        };
+
+        foreach (var testCase in testCases)
+        {
+            var lexer = new Lexer(testCase.Input);
+            foreach (var expected in testCase.ExpectedTokens)
+            {
+                var token = lexer.GetNextToken();
+                Assert.Multiple(() =>
+                {
+                    Assert.That(token.Literal, Is.EqualTo(expected.Literal),"TestCase: '{0}'. Expected '{1}', got '{2}'", testCase.Input, expected.Literal, token.Literal);
+                    Assert.That(token.TokenType, Is.EqualTo(expected.TokenType), "TestCase: '{0}'. Expected '{1}, got '{2}'", testCase.Input, expected.TokenType, token.TokenType);
+                });
+            }
+        }
+    }
+
+    private static Token CreateToken(TokenType tokenType, string? literal = null)
+    {
+        return new Token(tokenType, literal);
+    }
+
+    private readonly struct TestCase 
+    {
+        public readonly string Input { get; }
+        public readonly Token[] ExpectedTokens { get; }
+
+        public TestCase(string input, Token[] expectedToken)
+        {
+            Input = input;
+            ExpectedTokens = expectedToken;
+        }
+    }
+}
---
+++ DotnetMailMerge/Markdown/Lexer.cs
@@ -0,0 +1,81 @@
+﻿namespace DotnetMailMerge.Markdown;
+
+public class Lexer
+{
+	private readonly string _input;
+	private char? _currentChar;
+	private int _readPos;
+	private char? _prevChar;
+
+	public Lexer(string input)
+	{
+		_input = input;
+		_readPos = -1;
+		_currentChar = null;
+		_prevChar = null;
+	}
+
+	private void ReadNextChar()
+	{
+		_readPos++;
+		_currentChar = _input.Length > _readPos ? _input[_readPos] : null;
+    }
+
+    public Token GetNextToken()
+	{
+		ReadNextChar();
+
+		var token = _currentChar switch { 
+			var a when (a == '#' && IsSpecial(a, '#')) => new Token(TokenType.Heading),
+			var a when (a == '*' && IsSpecial(a, null)) => new Token(TokenType.Item),
+			'\n' => new Token(TokenType.LineBreak),
+			_ => new Token(TokenType.Letter, _currentChar.ToString()),
+        };
+
+		return token;
+    }
+
+	private bool IsSpecial(char? a, char? allowedPrev)
+	{
+		if ( _prevChar == allowedPrev || _prevChar == null || _prevChar == '\n')
+		{
+			return true;
+        }
+		return false;
+    }
+
+	private bool IsAllowed(char? a)
+	{
+		return false;
+    }
+}
+
+public readonly struct Token 
+{ 
+	public readonly TokenType TokenType { get; }
+	public readonly string? Literal { get; }
+	public Token(TokenType tokenType, string? literal = null)
+	{
+		TokenType = tokenType;
+		Literal = literal;
+    }
+}
+
+public enum TokenType
+{
+    EOF,
+    Illegal,
+    Heading,
+    Letter,
+    LineBreak,
+    //OrderedItem,
+    Item,
+    LParen,
+    RParen,
+    RBracket,
+    LBracket,
+    Bang,
+}
+
+
+
