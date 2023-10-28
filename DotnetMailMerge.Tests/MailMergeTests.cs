d873b9f56a5e6f0e220367ff5238ea2d544fae44
Andreas Olsson <olsson.andreas@gmail.com>
2023-10-28 13:20:31 +0200

    renamed to MailMerge and removed dictionary based version

--- DotnetMailMerge.Tests/MailMergeTests.cs
+++ DotnetMailMerge.Tests/MailMergeTests.cs
@@ -1,82 +1,15 @@
 ﻿using NUnit.Framework;
 using DotnetMailMerge.Exceptions;
-using System;
-using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
 namespace DotnetMailMerge.Tests;
 
-public class MailMergeTests
+public class MailMergeTestsV2
 {
     [SetUp]
     public void Setup()
     {
-    }
-
-    [Test]
-    public void Test()
-    {
-        Dictionary<string, object> obj = new()
-        {
-            {
-                 "listOfObjects", new object[] { new { show = true, A = 1 }, new { show = false, A = 2 } } 
-            },
-            {
-                "listOfInts", new int[] { 1, 2, 3}
-            },
-            {
-                "listOfStrings", new string[] {"A", "B", "C"}
-            },
-            {
-                "A", "B"
-            },
-            {
-                "IsValid", true
-            }
-        };
-
-        var jsonString = JsonSerializer.Serialize(obj);
-
-        //var jObj = obj as JsonObject;
-        var jNode = JsonNode.Parse(jsonString);
-        var jObj = JsonNode.Parse(jsonString).AsObject();
-        var list = jObj["listOfInts"];
-    }
-
-    [Test]
-    public void MarkdownParagraphReplace()
-    {
-        var template = @"<body><div>{{{ paragraph }}}</div></body>";
-        var expected = @"<body><div><p>Lorem ipsum</p></div></body>";
-
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() { { "paragraph", "Lorem ipsum"} });
-
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void MarkdownItemReplace()
-    {
-        var template = @"<body><div>{{{ items }}}</div></body>";
-        var expected = @"<body><div><ul><li>A</li><li>B</li></ul></div></body>";
-
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() { { "items", "* A\n* B"} });
-
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
-    }
-    [Test]
-    public void MarkdownReplaceHeadingAndParagraph()
-    {
-        var template = "<body><div>{{{ content }}}</div></body>";
-        var expected = "<body><div><h1>Heading</h1><p>Lorem ipsum</p></div></body>";
-
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() { { "content", "# Heading\nLorem ipsum"} });
-
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
     }
 
     [Test]
@@ -88,7 +21,7 @@ public class MailMergeTests
         var sut = new MailMerge(template);
         var result = sut.Render(new() { { "title", "Title" } });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -100,7 +33,7 @@ public class MailMergeTests
         var sut = new MailMerge(template);
         var result = sut.Render(new() { { "title", "Title" } });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
 
@@ -116,23 +49,17 @@ public class MailMergeTests
             { "body", "Body" }
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void StringReplacement_NotAllReplaced_ReturnsError()
+    public void StringReplacement_NotAllReplaced_ThrowsMissingParameterException()
     {
         var template = @"<html><body><h1>{{title}}</h1><p>{{body}}</p></body></html>";
 
         var sut = new MailMerge(template);
-        var result = sut.Render(new() { { "title", "Title" } });
 
-        var error = result.Match(success => new Exception("Unexpected Success"), err => err);
-        Assert.Multiple(() =>
-        {
-            Assert.That(result.IsError, Is.True);
-            Assert.That(error, Is.TypeOf<MissingParameterException>());
-        });
+        Assert.Throws<MissingParameterException>(() => sut.Render(new() { { "title", "Title" } }));
     }
 
     [Test]
@@ -147,70 +74,7 @@ public class MailMergeTests
             { "show", true }
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void If_ObjectProperty_ConditionTrue()
-    {
-        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if someobj.show}}<p>Extra</p>{{/if}}</body></html>";
-        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p><p>Extra</p></body></html>";
-
-        var showJson = @"
-            {
-            ""show"": true
-            }";
-
-        var showObj = JsonSerializer.Deserialize<JsonElement>(showJson);
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() {
-            { "title", "Title" },
-            { "someobj", new Dictionary<string, object> { { "show", true } } },
-         });
-
-        Assert.That(result.Match(success => success, err => err.ToString()), Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void If_JsonElement_ObjectProperty_ConditionFalse()
-    {
-        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if someobj.show}}<p>Extra</p>{{/if}}</body></html>"; 
-        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p></body></html>";
-
-        var showJson = @"
-            {
-            ""show"": false
-            }";
-
-        var showObj = JsonSerializer.Deserialize<JsonElement>(showJson);
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() {
-            { "title", "Title" },
-            { "someobj", showObj },
-         });
-
-        Assert.That(result.Match(success => success, err => err.ToString()), Is.EqualTo(expected));
-    }
-
-    [Test]
-    public void If_JsonElement_ObjectProperty_ConditionTrue()
-    {
-        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if someobj.show}}<p>Extra</p>{{/if}}</body></html>";
-        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p><p>Extra</p></body></html>";
-
-        var showJson = @"
-            {
-            ""show"": true
-            }";
-
-        var showObj = JsonSerializer.Deserialize<JsonElement>(showJson);
-        var sut = new MailMerge(template);
-        var result = sut.Render(new() {
-            { "title", "Title" },
-            { "someobj", showObj },
-         });
-
-        Assert.That(result.Match(success => success, err => err.ToString()), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -225,22 +89,49 @@ public class MailMergeTests
             { "show", false }
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void If_EmptyStringCondition_EvaluatesAsFalse()
-    { 
-        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if show}}<p>Extra</p>{{/if}}</body></html>"; 
-        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p></body></html>";
+    public void If_ObjectProperty_ConditionTrue()
+    {
+        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if someobj.show}}<p>Extra</p>{{/if}}</body></html>";
+        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p><p>Extra</p></body></html>";
 
+        var showJson = @"
+            {
+            ""show"": true
+            }";
+
+        var showObj = JsonSerializer.Deserialize<JsonNode>(showJson);
         var sut = new MailMerge(template);
-        var result = sut.Render(new() { 
+        var result = sut.Render(new() {
             { "title", "Title" },
-            { "show", "" }
+            { "someobj", showObj },
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void If_ObjectProperty_ConditionFalse()
+    {
+        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if someobj.show}}<p>Extra</p>{{/if}}</body></html>"; 
+        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p></body></html>";
+
+        var showJson = @"
+            {
+            ""show"": false
+            }";
+
+        var showObj = JsonSerializer.Deserialize<JsonNode>(showJson);
+        var sut = new MailMerge(template);
+        var result = sut.Render(new() {
+            { "title", "Title" },
+            { "someobj", showObj },
+         });
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -255,7 +146,22 @@ public class MailMergeTests
             { "show", "a" }
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void If_EmptyStringCondition_EvaluatesAsFalse()
+    { 
+        var template = @"<html><body><h1>{{title}}</h1><p>Lorem ipsum</p>{{#if show}}<p>Extra</p>{{/if}}</body></html>"; 
+        var expected = @"<html><body><h1>Title</h1><p>Lorem ipsum</p></body></html>";
+
+        var sut = new MailMerge(template);
+        var result = sut.Render(new() { 
+            { "title", "Title" },
+            { "show", "" }
+         });
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -271,7 +177,7 @@ public class MailMergeTests
             { "shownested", true },
          });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -280,16 +186,9 @@ public class MailMergeTests
         var template = @"<html><body>{{#if show}}<p>Extra</p>{{/if}}</body></html>"; 
 
         var sut = new MailMerge(template);
-        var result = sut.Render(new() { 
+        Assert.Throws<ConditionException>(() => sut.Render(new() { 
             { "show", 1 }
-         });
-
-        var error = result.Match(success => new Exception("Unexpected Success"), err => err);
-        Assert.Multiple(() =>
-        {
-            Assert.That(result.IsError, Is.True);
-            Assert.That(error, Is.TypeOf<ConditionException>());
-        });
+         }));
     }
 
     [Test]
@@ -305,7 +204,7 @@ public class MailMergeTests
             { "show", false },
         });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -321,7 +220,7 @@ public class MailMergeTests
             { "show", true },
         });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -338,7 +237,7 @@ public class MailMergeTests
             { "text", "Lorem ipsum" },
         });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -355,7 +254,7 @@ public class MailMergeTests
             { "some.text", "Lorem ipsum" },
         });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -372,39 +271,43 @@ public class MailMergeTests
             { "esome.text", "Lorem ipsum" },
         });
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void Loop_ListOfInts_WithoutAccessingProperties()
+    public void MarkdownParagraphReplace()
     {
-        var template = "{{#each items}}<p>Item</p>{{/each}}";
-        var expected = "<p>Item</p><p>Item</p>";
+        var template = @"<body><div>{{{ paragraph }}}</div></body>";
+        var expected = @"<body><div><p>Lorem ipsum</p></div></body>";
 
         var sut = new MailMerge(template);
+        var result = sut.Render(new() { { "paragraph", "Lorem ipsum"} });
 
-        var result = sut.Render(new()
-        {
-            { "items", new int[] {1, 2} }
-        });
-
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
-    public void Loop_ListOfInts_AccessingValue()
+    public void MarkdownItemReplace()
     {
-        var template = "{{#each items}}<p>{{this}}</p>{{/each}}";
-        var expected = "<p>1</p><p>2</p>";
+        var template = @"<body><div>{{{ items }}}</div></body>";
+        var expected = @"<body><div><ul><li>A</li><li>B</li></ul></div></body>";
 
         var sut = new MailMerge(template);
+        var result = sut.Render(new() { { "items", "* A\n* B"} });
 
-        var result = sut.Render(new()
-        {
-            { "items", new int[] {1, 2} }
-        });
+        Assert.That(result, Is.EqualTo(expected));
+    }
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+    [Test]
+    public void MarkdownReplaceHeadingAndParagraph()
+    {
+        var template = "<body><div>{{{ content }}}</div></body>";
+        var expected = "<body><div><h1>Heading</h1><p>Lorem ipsum</p></div></body>";
+
+        var sut = new MailMerge(template);
+        var result = sut.Render(new() { { "content", "# Heading\nLorem ipsum"} });
+
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -415,13 +318,16 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
+        var json = @"
         {
-            { "someobj", new Dictionary<string, object>() {
-                { "someprop", "test" }
-                }
-            }});
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+            ""someobj"": {
+                ""someprop"": ""test""
+            }
+
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -432,17 +338,52 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
+        var json = @"
         {
-            { "someobj", new Dictionary<string, object>()
-                {
-                    { "someprop", "test" },
-                }
+            ""someobj"": {
+                ""someprop"": ""test""
             }
-        });
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
+        Assert.That(result, Is.EqualTo(expected));
     }
 
+    [Test]
+    public void Loop_ListOfInts_WithoutAccessingValue()
+    {
+        var template = "{{#each items}}<p>Item</p>{{/each}}";
+        var expected = "<p>Item</p><p>Item</p>";
+
+        var sut = new MailMerge(template);
+
+        var json = @"{
+            ""items"": [1,2]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+
+        var result = sut.Render(jsonObj);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
+
+    [Test]
+    public void Loop_ListOfInts_AccessingValue()
+    {
+        var template = "{{#each items}}<p>{{this}}</p>{{/each}}";
+        var expected = "<p>1</p><p>2</p>";
+
+        var sut = new MailMerge(template);
+
+        var json = @"{
+            ""items"": [1,2]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
+
+        Assert.That(result, Is.EqualTo(expected));
+    }
 
     [Test]
     public void Loop_ListOfObject_WithoutAccessingProperties()
@@ -452,12 +393,13 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
-        {
-            { "items", new object[] { new { A = 1 }, new { A = 2 } } }
-        });
+        var json = @"{
+            ""items"": [{ ""A"": 1 }, { ""A"": 2 }]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -468,12 +410,13 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
-        {
-            { "items", new object[] { new { A = 1 }, new { A = 2 } } }
-        });
+        var json = @"{
+            ""items"": [{ ""A"": 1 }, { ""A"": 2 }]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -484,13 +427,14 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
-        {
-            { "items", new object[] { new { A = 1 }, new { A = 2 } } },
-            { "text", "text" },
-        });
+        var json = @"{
+            ""text"": ""text"",
+            ""items"": [{ ""A"": 1 }, { ""A"": 2 }]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -501,12 +445,13 @@ public class MailMergeTests
 
         var sut = new MailMerge(template);
 
-        var result = sut.Render(new()
-        {
-            { "items", new object[] { new { show = true, A = 1 }, new { show = false, A = 2 } } },
-        });
+        var json = @"{
+            ""items"": [{ ""show"": true, ""A"": 1 }, { ""show"": false, ""A"": 2 }]
+        }";
+        var jsonObj = JsonSerializer.Deserialize<JsonObject>(json);
+        var result = sut.Render(jsonObj);
 
-        Assert.That(result.Match(success => success, _ => ""), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 
     [Test]
@@ -515,14 +460,14 @@ public class MailMergeTests
         var template = "{{#each items}}{{#if this.show}}<p>text</p>{{/if}}{{/each}}";
         var expected = "<p>text</p>";
 
-        var showJson = @"[ { ""show"": true } ]";
-
-        var showObj = JsonSerializer.Deserialize<JsonElement>(showJson);
         var sut = new MailMerge(template);
-        var result = sut.Render(new() {
-            { "items", showObj },
-         });
+        var showJson = @"
+        {
+            ""items"": [ { ""show"": true } ]
+        }";
+        var showObj = JsonSerializer.Deserialize<JsonObject>(showJson);
+        var result = sut.Render(showObj);
 
-        Assert.That(result.Match(success => success, err => err.ToString()), Is.EqualTo(expected));
+        Assert.That(result, Is.EqualTo(expected));
     }
 }
